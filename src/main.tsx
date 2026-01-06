4693a3e496426050b06faed05cd47653e8ee375b
trimstrayy <aayuud9@gmail.com>
2026-01-06 22:17:05 +0545

    vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+import { Analytics } from "@vercel/analytics/react";
+
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    <Analytics />
+  </>
+);
